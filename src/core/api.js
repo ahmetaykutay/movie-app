08b1092126bae8d6c1c6052f107ebbcfd547623c
ahmet aykut ay <ahmetaykutay@ahmets-MacBook-Air.local>
2019-05-24 11:03:07 +0300

    detail page - plot text fetch

--- src/core/api.js
+++ src/core/api.js
@@ -1,11 +1,7 @@
 const BASE_URL = 'http://www.omdbapi.com/'
 const API_KEY = '1a7989f3'
 
-function search(searchString, page = 1) {
-	const querySting = [['apikey', API_KEY], ['s', searchString], ['page', page]]
-		.map(queryData => queryData.join('='))
-		.join('&')
-	const url = `${BASE_URL}?${querySting}`
+function get(url) {
 	return fetch(url, {
 		method: 'GET'
 	}).then(res => {
@@ -13,6 +9,20 @@ function search(searchString, page = 1) {
 	})
 }
 
+function search(searchString = '', page = 1) {
+	const querySting = [['apikey', API_KEY], ['s', searchString], ['page', page]]
+		.map(queryData => queryData.join('='))
+		.join('&')
+	const url = `${BASE_URL}?${querySting}`
+	return get(url)
+}
+
+function getById(id) {
+	return get(`${BASE_URL}?i=${id}&apikey=${API_KEY}`)
+}
+
 export default {
-	search
+	search,
+	getById,
+	get
 }
--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import classes from './Detail.module.scss'
+import api from '../../core/api'
 
-export default function Detail({ location }) {
+export default function Detail({ location, match }) {
+	const [plot, setPlot] = useState('')
 	const { data } = location.state
 	if (!data) {
 		return (
@@ -10,11 +12,22 @@ export default function Detail({ location }) {
 			</div>
 		)
 	}
+
+	const fetchData = async () => {
+		const response = await api.getById(match.params.imdbID)
+		setPlot(response.Plot)
+	}
+
+	useEffect(() => {
+		fetchData()
+		return () => {}
+	}, [])
+
 	return (
 		<div className="page">
 			<img className={classes.poster} src={data.Poster} />
 			<h1 className={classes.title}>{data.Title}</h1>
-			<p className={classes.plot}>{data.Plot}</p>
+			<p className={classes.plot}>{plot}</p>
 		</div>
 	)
 }
