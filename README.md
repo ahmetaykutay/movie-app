c8f8c60fb5feac95f8c3f75004c247f883b5e337
ahmet aykut ay <ahmetaykutay@ahmets-MacBook-Air.local>
2019-05-24 03:26:16 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Movie App
+
+simple react/redux movie app.
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+	return <p>hello world</p>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
